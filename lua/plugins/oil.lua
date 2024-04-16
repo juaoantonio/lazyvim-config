3077e76680323e023261da7311dc96b173f4f8dd
João Antônio Barbosa <joaobarbosadev@gmail.com>
2024-04-16 02:40:36 -0300

    initial files

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,19 @@
+return {
+  "stevearc/oil.nvim",
+  event = "VimEnter", -- Sets the loading event to 'VimEnter'
+  opts = {
+    delete_to_trash = true,
+  },
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local oil = require("oil")
+    oil.setup()
+    vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end,
+  init = function()
+    if vim.fn.argc() == 0 then
+      require("oil").open()
+    end
+  end,
+}
